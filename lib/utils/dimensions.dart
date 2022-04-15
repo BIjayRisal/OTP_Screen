add9d61bcc8b57f79cadbd39a599d04d2bb3cbc2
BijayRisal <bijayrisal1@gmail.com>
2022-04-15 16:07:33 +0545

    few changes

---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,58 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+class Dimensions
+{
+static double screenHeight =  Get.context!.height;
+static double screenWidth =  Get.context!.width;
+
+
+
+static double pageViewContainer = screenHeight/3.652;
+static double pageView = screenHeight/2.511;
+static double pageViewTextContainer = screenHeight/6.18;
+
+static double height10 = screenHeight/80.36;
+static double height15 = screenHeight/53.57;
+static double? height20 = screenHeight/40.18;
+static double height30 = screenHeight/26.78;
+static double height40 = screenHeight/20.09;
+static double height35 = screenHeight/22.95;
+static double height45 = screenHeight/17.85;
+static double height50 = screenHeight/16.065;
+static double height100 = screenHeight/8.036;
+static double height220 = screenHeight/3.652;
+
+
+static double width10 = screenWidth/39.272;
+static double width20 = screenWidth/19.536;
+static double width30 = screenWidth/13.090;
+static double width45 = screenWidth/7.854;
+static double width50 = screenWidth/8.72;
+static double width90 = screenWidth/4.36;
+static double width150 = screenWidth/2.616;
+static double width320 = screenWidth/1.22;
+static double width340 = screenWidth/1.115;
+static double width350 = screenWidth/1.211;
+
+static double radius20 = screenHeight/40.18;
+static double radius10 = screenHeight/80.36;
+static double radius15 = screenHeight/53.57;
+
+//popular food
+static double popularFoodImgSize = screenHeight/2.296;
+
+//icon size
+static double iconSize16 = screenHeight/50.22;
+static double iconSize24 = screenHeight/33.84;
+
+//font size
+static double fontSize20 = screenHeight/40.18;
+static double fontSize25 = screenHeight/32.144;
+static double fontSize15 = screenHeight/53.57;
+static double fontSize26 = screenHeight/30.90;
+static double fontSize16 = screenHeight/50.22;
+
+// bottom height
+static double bottomHeightBar = screenHeight/6.96;
+}
